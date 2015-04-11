0cfd9fdcb484e2626294caad5c57580d6dbbb42b
Amy Simmons <amy__simmons@hotmail.com>
2015-04-11 23:42:55 +1000

    bubble sorting in progress

---
+++ bubble-sort-js/bubble-sort.js
@@ -0,0 +1,12 @@
+// The bubble sort algorithm:
+// Compare adjacent elements. If the first is greater than the second, swap them.
+
+// Do this for each pair of adjacent elements, starting with the first two and ending 
+// with the last two. At this point the last element should be the greatest. 
+
+// Repeat the steps for all elements except the last one. 
+
+// Continue for one less element each time, until there are no more pairs to compare.
+
+// console.log('hello world');
+
---
+++ bubble-sort-rb/bubble-sort.rb
@@ -0,0 +1,27 @@
+# The bubble sort algorithm:
+# Compare adjacent elements. If the first is greater than the second, swap them.
+
+# Do this for each pair of adjacent elements, starting with the first two and ending 
+# with the last two. At this point the last element should be the greatest. 
+
+# Repeat the steps for all elements except the last one. 
+
+# Continue for one less element each time, until there are no more pairs to compare.
+
+i = 0
+a = (1..100).to_a.shuffle
+count = a.length - 1
+
+until count == 0
+  while i < count
+    if a[i] > a[i+1]
+      a[i], a[i+1] = a[i+1], a[i]
+    end
+    i = i + 1
+  end
+  count = count - 1
+end
+
+
+
+
