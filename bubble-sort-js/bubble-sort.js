20e7339e3085bcc8c290e60c32be0eb62c8c7c3f
Amy Simmons <amy__simmons@hotmail.com>
2015-04-12 11:11:55 +1000

    bubble sorting with js

--- bubble-sort-js/bubble-sort.js
+++ bubble-sort-js/bubble-sort.js
@@ -10,22 +10,31 @@
 
 // console.log('hello world');
 
+var i = 0
+var a = [10, 5, 4, 3, 9, 8, 7, 1 , 2, 6];
+var count = a.length - 1
 
+console.log(a);
 
+while (count > 0) {
+  while (i < count){
+    if (a[i] > a[i+1]){
+      // hang onto the first value
+      var x = a[i];
+      // set the first value to equal the second value
+      a[i] = a[i+1];
+      // set the second value to equal the first value
+      a[i+1] = x;
+    }
+    // increment i to compare the next element in the array
+    i = i + 1;
+  }
+  // reset i to 0 so it starts comparing from the beginning
+  // of the array again
+  i = 0;
+  // minus 1 from count so it doesn't check elements which
+  // have already been sorted
+  count = count - 1;
+}
 
-
-// My ruby solution:
-// i = 0
-// a = (1..100).to_a.shuffle
-// count = a.length - 1
-
-// until count == 0
-//   while i < count
-//     if a[i] > a[i+1]
-//       a[i], a[i+1] = a[i+1], a[i]
-//     end
-//     i = i + 1
-//   end
-//   i = 0
-//   count = count - 1
-// end
+console.log(a);
