1bde43a7e6cc0889787be29632175b17452c41fa
Amy Simmons <amy__simmons@hotmail.com>
2015-05-01 00:54:13 +1000

    game over working

--- minesweeper/minesweeper.js
+++ minesweeper/minesweeper.js
@@ -1,126 +1,124 @@
-$(document).ready(function(){
+Minesweeper = {
 
-  Minesweeper = {
+  renderHeading: function(){
+    $('<h1></h1>').text('Minesweeper').appendTo('body');
+  },
 
-    renderHeading: function(){
-      $('<h1></h1>').text('Minesweeper').appendTo('body');
-    },
-
-    // render size selection form
-    renderForm: function(){
-      
-      var sizes = ["S", "M", "L"]
-
-      $('<form>').addClass('game-dimensions-form').appendTo('body');
-
-      for (var i = 0; i < sizes.length; i++) {
-        var size = sizes[i];
-        $('<label>').text(size).appendTo('.game-dimensions-form');
-        $('<input>').attr('type', 'radio').attr('name', 'size').attr('value', size).appendTo('.game-dimensions-form');
-      };
-
-      $('<submit>').addClass('size-subimt').appendTo('.game-dimensions-form').text('Submit');
-
-    },
-
-    // determine size of board
-    sizeOfBoard: function(){
-
-      var sizeChoice = $('input:checked').val();
-      var width;
-      var length;
-
-      if (sizeChoice === "S"){
-        width = 9;
-        length = 9;
-      }else if (sizeChoice === "M"){
-        width = 12;
-        length = 12;
-      }else if (sizeChoice === "L") {
-        width = 15;
-        length = 15;
-      }
-     
-      Minesweeper.renderGame(width, length);
-
-    },
-
-    // draw the board
-    renderGame: function(width, length){
-
-      $('.game-dimensions-form').remove();
-      $('<div></div>').addClass('board').appendTo('body');
-
-      _(length).times(function(){
-        $('<div>').addClass('row').appendTo('.board');
-      });
-
-      var dataID = 0;
-      _.each($('.row'), function(row){
-        _(width).times(function(){
-          dataID += 1;
-          $('<div>').addClass('square').attr('data-id', dataID).appendTo(row);
-        });
-      });
+  // render size selection form
+  renderForm: function(){
     
-    Minesweeper.placeMines();
+    var sizes = ["S", "M", "L"];
 
-    },
+    $('<form>').addClass('game-dimensions-form').appendTo('body');
 
-    // randomly place the mines
-    placeMines: function(){
+    for (var i = 0; i < sizes.length; i++) {
+      var size = sizes[i];
+      $('<label>').text(size).appendTo('.game-dimensions-form');
+      $('<input>').attr('type', 'radio').attr('name', 'size').attr('value', size).appendTo('.game-dimensions-form');
+    };
 
-      var mines = _.shuffle($('.square')).slice(0,10);
-      _.each(mines, function(mine){
-        $(mine).addClass('mine').html('<i class="fa fa-flag"></i>');
-      });
+    $('<submit>').addClass('size-subimt').appendTo('.game-dimensions-form').text('Submit');
 
-    },
+  },
 
-    // updates squares to reflect number of mines they are touching
-    updateSquareNumbers: function(){
+  // determine size of board
+  sizeOfBoard: function(){
 
-    },
+    var sizeChoice = $('input:checked').val();
+    var width;
+    var length;
 
-    // checks is player clicked on normal square, a mine, or placed a flag
-    checkPlayerGuess: function(){
-
-
-    },
-
-    showNumber: function(){
-
-
-    },
-
-    placeFlag: function(){
-
-
-    },
-
-    // shows all mines and squares, the game is over
-    gameOver: function(){
-
-
-    },
-
-    gameWon: function(){
-
-
-    },
-
-    newGame: function(){
-
-
+    if (sizeChoice === "S"){
+      width = 9;
+      length = 9;
+    }else if (sizeChoice === "M"){
+      width = 12;
+      length = 12;
+    }else if (sizeChoice === "L") {
+      width = 15;
+      length = 15;
     }
+   
+    Minesweeper.renderGame(width, length);
+
+  },
+
+  // draw the board
+  renderGame: function(width, length){
+
+    $('.game-dimensions-form').remove();
+    $('<div></div>').addClass('board').appendTo('body');
+
+    _(length).times(function(){
+      $('<div>').addClass('row').appendTo('.board');
+    });
+
+    var dataID = 0;
+    _.each($('.row'), function(row){
+      _(width).times(function(){
+        dataID += 1;
+        $('<div>').addClass('square').attr('data-id', dataID).appendTo(row);
+      });
+    });
+  
+  Minesweeper.placeMines();
+
+  },
+
+  // randomly place the mines
+  placeMines: function(){
+
+    var mines = _.shuffle($('.square')).slice(0,10);
+    _.each(mines, function(mine){
+      $(mine).addClass('mine').addClass('hidden').html('<i class="fa fa-bolt"></i>');
+    });
+
+  },
+
+  // updates squares to reflect number of mines they are touching
+  updateSquareNumbers: function(){
+
+  },
+
+  // checks is player clicked on normal square, a mine, or placed a flag
+  checkPlayerGuess: function(){
+
+  },
+
+  showNumber: function(){
+
+  },
+
+  placeFlag: function(){
+
+  },
+
+  // shows all mines and squares, the game is over
+  gameOver: function(){
+    console.log('mine clicked');
+    $('.mine').removeClass('hidden');
+    console
+  },
+
+  gameWon: function(){
+
+  },
+
+  newGame: function(){
 
   }
+
+}
+
+$(document).ready(function(){
 
   Minesweeper.renderHeading();
   Minesweeper.renderForm();
 
   $('submit').on('click', Minesweeper.sizeOfBoard);
-
+  $('body').on('click', '.mine', function(){
+    Minesweeper.gameOver();
+  });
 
 });
 
